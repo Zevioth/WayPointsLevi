978910d9b8601d47ace987a6e83687b9b99b5537
Zevioth <zevioth@gmail.com>
2026-09-17 20:57:01 +0800

    Add files via upload

---
+++ src/config_generator.cpp
@@ -0,0 +1,30 @@
+#include "mod/Config.h"
+
+#include <pl/Config.hpp>
+
+#include <cstdio>
+#include <filesystem>
+
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    std::fprintf(stderr, "usage: %s <output-directory>\n", argv[0]);
+    return 1;
+  }
+
+  std::filesystem::path outDir(argv[1]);
+  std::error_code ec;
+  std::filesystem::create_directories(outDir, ec);
+  if (ec) {
+    std::fprintf(stderr, "failed to create %s: %s\n", outDir.string().c_str(), ec.message().c_str());
+    return 1;
+  }
+
+  pl::config::ConfigFile<waypointmanager::ModConfig> config(
+      waypointmanager::ModConfig{}, outDir / "config.json", outDir / "config.schema.json");
+  if (!config.load()) {
+    std::fprintf(stderr, "failed to generate default config\n");
+    return 1;
+  }
+  config.save();
+  return 0;
+}
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include "mod/MyMod.h"
+
+#include <pl/Mod.hpp>
+
+PL_REGISTER_MOD(waypointmanager::WaypointManagerMod, waypointmanager::WaypointManagerMod::instance());
